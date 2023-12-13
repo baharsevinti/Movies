50fd9e4678e6c0de6c568f9222a933094d56b5df
Bahar Sevinti <baharsevintii@gmail.com>
2023-12-13 17:35:47 +0300

    init

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export default axios.create({
-    baseURL:'https://raw.githubusercontent.com/fhsinchy/movieist/master/',
+    baseURL:'http://localhost:8080',
     headers:{'Access-Control-Allow-Origin': '*',}
 });
